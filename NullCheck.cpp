b3d43fe8e683b944bccbed7247ca7086ca8a6e69
4D-09\koyo <a>
2026-06-09 14:17:03 +0900

    いらない物は move で所有権を移し、無駄を減らす。 こうすることで、メモリの使用効率が上がる

--- NullCheck.cpp
+++ NullCheck.cpp
@@ -1,6 +1,47 @@
 ﻿#include <iostream>
+#include <assert.h>
+
+class Weapon
+{
+public:
+    void Use() {}
+};
+
+class Player
+{
+    std::shared_ptr<Weapon> weapon;
+    
+public:
+    Player(std::shared_ptr<Weapon> weapon1) : weapon(std::move(weapon1)) 
+    {
+		std::cout << weapon.use_count() << std::endl;
+        std::cout << weapon1.use_count() << std::endl;
+    }
+    /* いらない物は move で所有権を移し、無駄を減らす。
+       こうすることで、メモリの使用効率が上がる*/
+
+    void Attack()
+    {
+        if (weapon) weapon->Use();
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+ //   auto wp = std::make_shared<Weapon>();
+
+ //   // コピー(参照カウント +1)
+ //   Player p(wp);
+ //   // ムーブ(所有権を移動)
+	//Player q(std::move(wp));
+
+    auto a = std::make_shared<Weapon>();
+    auto b = a;
+    auto c = std::move(a);
+
+    Player l(a);
+    Player m(b);
+    Player n(c);
+
+    return 0;
 }
